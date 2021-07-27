b193c7127de0424adca9c43d396197ddc0728d10
ivanbodurov <85461420+ivanbodurov@users.noreply.github.com>
2021-07-27 15:19:34 +0300

    Create product-list.js

---
+++ product-list.js
@@ -0,0 +1,21 @@
+
+// Списък с продукти на AngularJS
+var app = angular.module("myShoppingList", []);
+app.controller("myCtrl", function($scope) {
+    $scope.products = ["Хляб", "Сирене", "Кисело мляко", "Сапун", "Паста за зъби", "Яйца", "Душгел", "Маслини", "Маргарин", "Кашкавал", "Лютеница", "Прясно мляко", "Кайма"];
+    $scope.addItem = function () {
+        $scope.errortext = "";
+        if (!$scope.addMe) {return;}
+        if ($scope.products.indexOf($scope.addMe) == -1) {
+            $scope.products.push($scope.addMe);
+            document.getElementById('new-product').value = "";
+
+        } else {
+            $scope.errortext = "Вече има такъв продукт.";
+        }
+    }
+    $scope.removeItem = function (x) {
+        $scope.errortext = "";
+        $scope.products.splice(x, 1);
+    }
+});
